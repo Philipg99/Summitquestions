42dc86b767c3026f980694c03fc8da0a4e126bed
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2019-03-23 16:26:00 +0530

    Add files via upload

--- Greater Number/Greater Number.py
+++ Greater Number/Greater Number.py
@@ -3,29 +3,19 @@ import math
 x = list(map(int,input().split()))
 y = list(map(int,input().split()))
 
-sx=1
-sy=1
+log_value_x = x[1]
+log_value_y = y[1]
+
+sign_x = 1
+sign_y = 1
 
 if x[0]<0:
-	x[0]*=-1
-	if x[1]%2!=0:
-		sx=-1
+	x[0]=abs(x[0])
+	sign_x = -1 if x[1]%2!=0 else 1
 
 if y[0]<0:
-	y[0]*=-1
-	if y[1]%2!=0:
-		sy=-1
+	y[0]=abs(y[0])
+	sign_y = -1 if y[1]%2!=0 else 1
 
-if x[0]==0 and y[0]==0:
-	print(0)
+if 
 
-elif x[0]==0:
-	print(1)
-
-elif y[0]==0:
-	print(0)
-
-elif sx*x[1]*math.log(x[0],10) >= sy*y[1]*math.log(y[0],10):
-	print(0)
-else:
-	print(1)
