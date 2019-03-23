e63ee3a062fcf147593d899a5b94ad31bdf27fe2
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2019-03-23 20:00:19 +0530

    Add files via upload

--- Greater Number/Greater Number.py
+++ Greater Number/Greater Number.py
@@ -3,12 +3,11 @@ import math
 x = list(map(int,input().split()))
 y = list(map(int,input().split()))
 
-log_value_x = x[1]
-log_value_y = y[1]
 
 sign_x = 1
 sign_y = 1
 
+#if the base is negative
 if x[0]<0:
 	x[0]=abs(x[0])
 	sign_x = -1 if x[1]%2!=0 else 1
@@ -17,5 +16,31 @@ if y[0]<0:
 	y[0]=abs(y[0])
 	sign_y = -1 if y[1]%2!=0 else 1
 
-if 
+#------------------------
 
+# if either one of the bases are zero
+if x[0] == 0 or y[0] == 0:
+	if sign_x*x[0]*x[1] >= sign_y*y[0]*y[0]:
+		print(0)
+	else:
+		print(1)
+
+#------------------------------------
+
+else:
+	# if rasied to zero
+	if x[1]!=0:
+		valx=sign_x*x[1]*math.log(x[0])
+	else:
+		valx=1
+	
+	if y[1]!=0:
+		valy=sign_y*y[1]*math.log(y[0])
+	else:
+		valy=1
+	#--------------------
+	
+	if valx >= valy:
+		print(0)
+	else:
+		print(1)
